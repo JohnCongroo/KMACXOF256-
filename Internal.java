900c7f6d376f42bfddd5c6863810ea3791a2400f
jsel127 <130511798+jsel127@users.noreply.github.com>
2024-03-28 22:20:05 -0700

    Update Internal.java
    bytepad, encode_string, right_encode

--- Internal.java
+++ Internal.java
@@ -1,5 +1,105 @@
 public class Internal { 
+	public static byte[] bytepad(byte[] X, int w) {
+		if (w > 0) {
+			byte[] left = left_encode(w);
+			byte[] z = new byte[left.length + X.length];
+			
+			// 1. z = left_encode(w) || X.
+			int index = 0;
+			for (byte encode: left) {
+				z[index] = encode;
+				index++;
+			}
+			for (byte encode: X) {
+				z[index] = encode;
+				index++;
+			}
+			
+			// 2. while len(z) mod 8 ≠ 0
+			if (z.length % 8 != 0) {
+				index = 0;
+				byte[] temp = new byte[z.length + 1];
+				for (byte encode: z) {
+					temp[index] = encode;
+				}
+				temp[temp.length - 1] = 0;
+				z = temp;
+			}
+			//3. while (len(z)/8) mod w ≠ 0:
+			if (z.length/8 % w != 0) {
+				index = 0;
+				byte[] temp = new byte[z.length + 1];
+				for (byte encode: z) {
+					temp[index] = encode;
+				}
+				temp[temp.length - 1] = 00000000;
+				z = temp;
+			}
+			return z;
+		} else {
+			System.out.println("fail");
+            return new byte[] {1};
+		}
+	}
+	
+	public static byte[] encode_string(byte[] S) {
+		if (0 <= S.length && S.length < Math.pow(2, 2040)) {
+			byte[] left = left_encode(S.length);
+			byte[] byteString = new byte[left.length + S.length];
+			
+			
+			// 1. Return left_encode(len(S)) || S.
+			int index = 0;
+			for (byte encode: left) {
+				byteString[index] = encode;
+				index++;
+			} 
+			for (byte encode: S) {
+				byteString[index] = encode;
+				index++;
+			} 
+			return byteString;
+		} else {
+            System.out.println("fail");
+            return new byte[] {1};
+        }
+	}
+	
+	public static byte[] right_encode(int x) {
+        //validate input
+        if (x >= 0 && x < Math.pow(2,2040)) {
 
+            //1. Let n be the smallest positive integer for which 28n > x
+            int n = 1;
+            while (Math.pow(2, 8 * n) <= x){
+                n++;
+            }
+
+            //System.out.println("aegoihjaeg");
+            //System.out.println(n);
+
+            //2. Let x1, x2, …, xn be the base-256...
+            int[] xi = new int[n + 1];
+            for (int i = n; i >= 1; i--){
+                xi[i] = (x % 256);
+                x = x / 256;
+            }
+
+            //3. Let Oi = enc8(xi), for i = 1 to n.
+            byte[] byteString = new byte[n + 1];
+            for (int i = 0; i < n; i++){
+                byteString[i] = (byte) xi[i];
+            }
+
+            //4. Let On+1 = enc8(n).
+            byteString[n] = (byte) n;
+            return byteString;
+        } else {
+            System.out.println("fail");
+            return new byte[] {1};
+        }
+	}
+    
     public static byte[] left_encode(int x){
         //validate input
         if (x>= 0 && x < Math.pow(2,2040)) {
