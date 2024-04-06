11045a46fd7ab509ed6e6e9bbf0cc790c7fe9c7a
Max Yim <johncongroo@gmail.com>
2024-04-06 14:23:20 -0700

    theta
    first draft theta implementation

--- Sha3.java
+++ Sha3.java
@@ -29,28 +29,63 @@ public class Sha3 {
     //a lot of these are constants for sha3 or keccak in particular
 
     //set by keccak
-    private static int rows = 5;
-    private static int columns = 5; 
+    private static int ROWS = 5;
+    private static int COLUMNS = 5; 
     //w, how long each lane is
     
     
 
-    private static int w = b/25; //should be 64 
-    private static int l = 6; //logbase2 of laneLength, log_2(64) = 6
+    private static int W = b/25; //should be 64 
+    private static int L = 6; //logbase2 of laneLength, log_2(64) = 6
 
     //initial state, no data is put in we do the permutations first then hash
-    static boolean[] state = new boolean[b]; //5 x 5 index, 64 deep
+    static boolean[] STATE = new boolean[b]; //5 x 5 index, 64 deep
 
     //A[x,y,z]=S[w(5y+x)+z].
 
-    private static int A(int x, int y, int z){
-        return (w* (5 * y + x) + z);
+    private static boolean A(int x, int y, int z){
+        return STATE[W * (5 * y + x) + z];
     }
+
+    private static boolean A(int x, int y, int z, boolean[] state){
+        return state[W * (5 * y + x) + z];
+    }
+
+
+    private static int getCoords(int x, int y, int z){
+        return W * (5 * y + x) + z;
+    }
+
+
 
     //keccac_p is permutations wiith a specific number of rounds, sha-3256 uses 1600 bits and 24 rounds, so thats what we'll use
     //reference: https://keccak.team/keccakp.html
-    public static void keccak_f(Object[] state){
+    public static void keccak_f(boolean[] state){
+
+
+            boolean[][] C = new boolean[5][W];
+            boolean[][] D = new boolean[5][W];
+            boolean[] tempState = state;
+
             //theta
+                for (int x = 0; x < 5; x++) {
+                    for(int z = 0; z < W; z++){
+                        C[x][z] = A(x, 0, z) ^ A(x, 1, z) ^ A(x, 2, z) ^ A(x, 3, z) ^ A(x, 4, z);
+                    }
+                }
+
+                for (int x = 0; x < 5; x++) {
+                    for(int z = 0; z < W; z++){
+                        D[x][z] = C[(x-1 + 5) % 5][z] ^ C[(x+1 + 5) % 5][(z-1 + W) % W];                    }
+                }
+
+                for (int x = 0; x < 5; x++){
+                    for (int y = 0; y < 5; y++){
+                        for(int z = 0; z < W; z++){
+                            tempState[getCoords(x, y, z)] = A(x, y, z, tempState) ^ D[x][z];   
+                        }
+                    }
+                }
 
             //rho
 
@@ -65,6 +100,9 @@ public class Sha3 {
     public static void main(String[] args) {
         //testing coordinates
         System.out.println(A(0,0,0));
+        System.out.println(A(0,1,1));
         System.out.println(A(4,0,63));
+
+        keccak_f(STATE);
     }
 }
